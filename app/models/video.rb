8299ae2ad3d24209e9b52158e9cdf2578f2cce68
Adam Hill <adamdh19@gmail.com>
2015-03-31 14:40:34 -0400

    initial commit.

--- app/models/video.rb
+++
@@ -1,3 +0,0 @@
-class Video < ActiveRecord::Base
-	acts_as_taggable
-end
--- db/migrate/20150331181120_create_videos.rb
+++
@@ -1,8 +0,0 @@
-class CreateVideos < ActiveRecord::Migration
-  def change
-    create_table :videos do |t|
-      t.string :video_title
-      t.text :embed_url
-    end
-  end
-end
